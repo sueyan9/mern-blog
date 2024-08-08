8f3284c7b3ce321dad19c4713f5b2767a1d16e21
sueyan9 <409133742@qq.com>
2024-08-08 21:25:37 +1200

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
 export default function App(){
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element ={<Home/>} />
-      <Route path="/about" element = {<About/>} />
+      <Route path="/about" element = {<About />} />
       <Route path= "/sign-in" element = {<SignIn />} />
       <Route path= "/sign-up" element = {<SignUp />} />
       <Route path= "/dashboard" element = {<Dashboard />} />
